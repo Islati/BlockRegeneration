a162fb68b7f2d0d0473db8e04b834d6fc8b12a4d
Brandon Curtis <brandon@caved.in>
2013-12-02 19:38:07 -0400

    Updated blocks being removed from explosions

--- src/main/java/com/caved_in/blockregeneration/listeners/BukkitListeners.java
+++ src/main/java/com/caved_in/blockregeneration/listeners/BukkitListeners.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 /**
  * Created By: TheGamersCave (Brandon)
@@ -40,5 +41,10 @@ public class BukkitListeners implements Listener {
 				event.setCancelled(true);
 			}
 		}
+	}
+
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent event) {
+		event.blockList().clear();
 	}
 }
